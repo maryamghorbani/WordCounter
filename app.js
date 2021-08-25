0f7d1f12d94b2575ff20fce38ef07aca5d4d8b97
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-25 22:38:53 +0430

    create server to show js file in browser on localhost

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const http = require('http');
+
+const server = http.createServer((request,response) => {
+    response.write("Hello world");
+    response.end();
+})
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log("server running on localhost:3000");
+})
