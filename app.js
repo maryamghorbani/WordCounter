ba9f8130ebdb75b54037e0327cc6b2b23a84df47
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-26 00:27:09 +0430

    a function that count words with regular expression

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const http = require('http');
+// const http = require('http');
+//
+// const server = http.createServer((request,response) => {
+//     response.write("Hello world");
+//     response.end();
+// })
+//
+// server.listen(3000, '127.0.0.1', () => {
+//     console.log("server running on localhost:3000");
+// })
 
-const server = http.createServer((request,response) => {
-    response.write("Hello world");
-    response.end();
-})
 
-server.listen(3000, '127.0.0.1', () => {
-    console.log("server running on localhost:3000");
-})
+function Count(text) {
+
+        var wordCount = text.match(/(\w+)/g).length;
+        // \w find characters and + continue until find non-alphanumeric character
+
+        console.log(wordCount)
+
+}
+Count("Hev % 1 3 cb&cv llo Maria dslkfm sdfjl lsfj m ")
