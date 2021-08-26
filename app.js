af5de0c46f64a344cd9641c522ad03eabb29d53a
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-08-26 14:36:09 +0430

    add text file to project directory and read text from it

--- app.js
+++ app.js
@@ -10,28 +10,39 @@
 // })
 
 
-const text = "salam man maryam hastam. salam. khjoobin? maryam salam";
-const CountUniqueWords = txt => new Set(txt.toLowerCase().match(/\w+/g)).size;
+
+var fs = require('fs');
+//allows to work with the file system on your computer.
+
+fs.readFile('file.txt', 'utf8', function(err, text) {
+
+    const CountUniqueWords = txt => new Set(txt.toLowerCase().match(/\w+/g)).size;
 //A Set is a collection of unique values. Each value may occur only once in a Set.
 // \w find characters and + continue until find non-alphanumeric character
 
-console.log(CountUniqueWords(text))
+    console.log(CountUniqueWords(text))
 
 
-var words = text.toLowerCase().match(/\w+/g).values();
-var items = {};
+    var words = text.toLowerCase().match(/\w+/g).values();
+    var items = {};
 //define a dictionary
-for (let item of words) {
-    if(isNaN(items[item])){
-        items[item] = 0;
+    for (let item of words) {
+        if(isNaN(items[item])){
+            items[item] = 0;
+        }
+        items[item] += 1;
     }
-    items[item] += 1;
-}
 // count unique words
 
-items = Object.keys(items).sort().reduce((r, k) => (r[k] = items[k], r), {});
+    items = Object.keys(items).sort().reduce((r, k) => (r[k] = items[k], r), {});
 // sort words
 
-for (const [key, value] of Object.entries(items)) {
-    console.log(key, value);
-}
+    for (const [key, value] of Object.entries(items)) {
+        console.log(key, value);
+    }
+
+
+});
+
+
+// const text = "salam man maryam hastam. salam. khjoobin? maryam salam";
